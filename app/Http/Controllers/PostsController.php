0873437fb7696c6741244af55004c26abecefc0b
vagrant <vagrant@homestead>
2018-02-20 08:33:34 +0000

    Create new post form functionality

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
@@ -10,8 +11,26 @@ class PostsController extends Controller
     {
         return view('posts.index');
     }
+
     public function show()
     {
         return view('posts.show');
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store()
+    {
+        $post = new Post;
+
+        $post->title = request('title');
+        $post->body = request('body');
+
+        $post->save();
+
+        return redirect('/');
     }
 }
---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,23 @@
+@extends ('layouts.master')
+
+@section ('content')
+
+<div class="col-sm-8">
+    <h1>Publish a post</h1>
+
+    <form method="POST" action="/posts">
+        {{ csrf_field() }}
+
+        <div class="form-group">
+            <label for="title">Title:</label>
+            <input type="text" class="form-control" name="title" id="title">
+        </div>
+        <div class="form-group">
+            <label for="body">Body</label>
+            <textarea  class="form-control" id="body" name="body"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+
+@endsection
