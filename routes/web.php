d30e1110071b89e0008ee4b1f090b3264d1933de
Anthony Parrett <anthonyparrett7@gmail.com>
2018-02-19 22:35:51 -0600

    Show each task on its own page

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
-Route::get('/index', function(){
+Route::get('/tasks', function(){
     
-    $tasks = DB::table('tasks')->get();
+    $tasks = DB::table('tasks')->latest()->get();
 
-    return view('index', compact('tasks'));
+    return view('tasks.index', compact('tasks'));
+});
+
+Route::get('/tasks/{task}', function($id) {
+    $task = DB::table('tasks')->find($id);
+
+    return view('tasks.show', compact('task'));
 });
