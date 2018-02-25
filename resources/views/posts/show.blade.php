3bb9434fac3c9895936feab165cb62f60aada34e
Anthony Parrett <anthonyparrett7@gmail.com>
2018-02-24 21:12:03 -0600

    Create comment functionality

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,4 +3,19 @@
 @section ('content')
     <h1>{{ $post->title }}</h1>
     <p>{{ $post->body }}</p>
+
+    <div class="comments">
+        <ul class="list-group">
+        @foreach ($post->comments as $comment)
+            <article>
+                <li class="list-group-item">
+                    <strong>
+                        {{ $comment->created_at->diffForHumans() }}
+                    </strong>
+                    <p>{{ $comment->body }}</p>
+                </li>
+            </article>
+        @endforeach
+        </ul>
+    </div>
 @endsection
