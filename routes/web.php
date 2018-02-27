258e03893f4bfd2f254338f71e21fc41bad71056
Anthony Parrett <anthonyparrett7@gmail.com>
2018-02-26 22:19:02 -0600

    Create registration and session controllers

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,6 @@ Route::get('/', 'PostsController@index');
 Route::get('/posts/create', 'PostsController@create');
 Route::post('/posts', 'PostsController@store');
 Route::get('/posts/{post}', 'PostsController@show');
+
+Route::get('/register', 'RegistrationController@create');
+Route::get('/login', 'SessionsController@create');
