52d5e4d813388375b13b947fad279d0e3111606d
Anthony Parrett <anthonyparrett7@gmail.com>
2018-03-01 19:43:46 -0600

    Create registration page and functionality

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -3,5 +3,26 @@
 @section ('content')
     <div class="col-sm-8">
         <h1>Register</h1>
+        <form method="POST" action="/register/create">
+            {{ csrf_field() }}
+
+            <div class="form-group">
+                <label for="name">name:</label>
+                <input type="text" class="form-control" name="name" id="name" >
+            </div>
+            <div class="form-group">
+                <label for="email">email</label>
+                <input type="email" class="form-control" id="email" name="email" >
+            </div>
+            <div class="form-group">
+                <label for="password">password</label>
+                <input type="password" class="form-control" id="password" name="password" >
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+
+            @include('layouts.errors')
+        </form>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-Route::get('/', 'PostsController@index');
+Route::get('/', 'PostsController@index')->name('home');
 Route::get('/posts/create', 'PostsController@create');
 Route::post('/posts', 'PostsController@store');
 Route::get('/posts/{post}', 'PostsController@show');
 
 Route::get('/register', 'RegistrationController@create');
+Route::post('/register/create', 'RegistrationController@store');
 Route::get('/login', 'SessionsController@create');
